8815ff6b8c165ab166a3799fbebccff5bd781c37
xiulinyang <xiulin.yang.compling@gmail.com>
2024-11-19 17:58:56 +0100

    update code

---
+++ run_one_language.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+bash run.sh RO 30000 5
+bash run.sh RO 50000 5
+
+bash run.sh TR 40000 5
+bash run.sh TR 50000 5
+
+bash run.sh EN 60000 5
+bash run.sh EN 70000 5
+bash run.sh TR 60000 5
+
+bash run.sh EN 80000 5
+
+bash run.sh DE 70000 5
+bash run.sh DE 80000 5
+
+bash run.sh RO 80000 5
+bash run.sh RU 80000 5
+
---
+++ run_tr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+bash run.sh EN 30000 0
+bash run.sh DE 30000 0
+
+bash run.sh TR 30000 0
+bash run.sh DE 40000 0
+bash run.sh DE 50000 0
+bash run.sh EN 40000 0
+
+bash run.sh RU 30000 0
+bash run.sh RU 40000 0
+bash run.sh RU 50000 0
+
+bash run.sh DE 60000 0
+bash run.sh RU 60000 0
+bash run.sh RO 60000 0
